a5e7381e37cb14e5016c0f144d50f2ccad3e9048
HariPrasath C <hariprasath.c@conservesolution.com>
2025-08-23 20:49:47 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,7 +14,8 @@ builder.Services.AddCors(options =>
             "http://localhost:3000",
             "http://localhost:3001",
             "http://localhost:3002",
-            "https://teams-meeting-6nqqa3u12-hari-dev-engs-projects.vercel.app"
+            "https://teams-meeting-6nqqa3u12-hari-dev-engs-projects.vercel.app",
+            "https://teams-meeting-web.vercel.app"
         )
         .AllowAnyHeader()
         .AllowAnyMethod();
